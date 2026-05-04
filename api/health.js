cdbee9b2034808422a58fd92ae9e2ae19f84d713
ohseasonhan-coder <ohseasonhan@gmail.com>
2026-05-04 16:15:44 +0900

    Delete api directory

--- api/health.js
+++
@@ -1,9 +0,0 @@
-import { json, optionsHandler } from "./_lib/quotes.js";
-
-export function OPTIONS() {
-  return optionsHandler();
-}
-
-export function GET() {
-  return json({ ok: true, service: "quotes", env: "vercel" });
-}
