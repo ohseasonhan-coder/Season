5b85260265d094bea2d88e927c3c0eb24af6c3a3
ohseasonhan-coder <ohseasonhan@gmail.com>
2026-05-08 13:11:43 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+ 
 export default defineConfig({
   plugins: [react()],
   base: "./",
@@ -16,3 +16,4 @@ export default defineConfig({
     chunkSizeWarningLimit: 3000
   }
 });
+ 
